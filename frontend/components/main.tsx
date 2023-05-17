7df9a74f568f5a6c21a831c0bb55f7cf1b84698e
natchy <natchjirawat@gmail.com>
2023-05-17 22:21:37 +0700

    updated

--- frontend/components/main.tsx
+++ frontend/components/main.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import Measurement from "./measurement"
-import PopUp from "./tools/popup"
+import PopUp from "./tools/modal"
 import Sensor from "./sensor"
 
 const Main = () => {
@@ -8,10 +8,10 @@ const Main = () => {
         <div className="">
             <div className="flex items-center justify-between text-white py-4 px-8 bg-red-400">
                 {/* Headline */}
-                <h1 className="text-3xl font-bold">Raining Prediction</h1>
-                <div className="flex-grow"></div>
-                <div className="flex items-center space-x-8">
-                    <h1 className="text-xl font-bold mr-10">Status</h1>
+                <h1 className="text-3xl font-bold">Should I water the plants ?</h1>
+                <div className="flex"></div>
+                <div className="flex items-center justify-items-center gap-[17rem] px-8 ">
+                    <h1 className="text-xl font-bold mx-4">Status</h1>
                     <h1 className="text-xl font-bold mr-10">Value</h1>
                 </div>
                 <PopUp />
@@ -19,7 +19,7 @@ const Main = () => {
 
 
             {/* Sensor Value */}
-            <div id="sensor">
+            <div id="sensor" className="py-4">
                 <Sensor />
             </div>
             {/* Measurement */}
--- frontend/components/sensor.tsx
+++ frontend/components/sensor.tsx
@@ -3,43 +3,55 @@ import React, { useState } from 'react';
 const Sensor = () => {
     return (
         <div className='justify-between space-y-2'>
-            <h2 className='text-2xl font-bold'>Sensor</h2>
+            <h2 className='text-2xl font-bold ml-10'>Sensor</h2>
             <div className=''>
                 {/* Temperature */}
-                <section id="temperature" className="flex py-8 justify-between">
-                    <h2 className='ml-10'>Temperature</h2>
-                    <p className='mr-10'>100</p>
+                <section id="temperature" className="flex px-8 py-8 justify-around">
+                    <h2 className='items-center justify-center'>Temperature</h2>
+                    <p className=''>100</p>
                     <p>200</p>
                 </section>
 
                 {/* Humidity component */}
-                <section id="humidity" className="flex py-8">
+                <section id="humidity" className="flex px-8 py-8 justify-around">
                     <h2>Humidity</h2>
+                    <p className='mr-10'>100</p>
+                    <p>200</p>
                 </section>
 
                 {/* Windspeed Component */}
-                <section id="windspeed" className="flex py-8">
+                <section id="windspeed" className="flex py-8 justify-around">
                     <h2>Windspeed</h2>
+                    <p className='mr-10'>100</p>
+                    <p>200</p>
                 </section>
 
                 {/* Rain Meter */}
-                <section id="temperature" className="flex py-8">
+                <section id="temperature" className="flex py-8 justify-around">
                     <h2>Rain Meter</h2>
+                    <p className='mr-10'>100</p>
+                    <p>200</p>
                 </section>
 
                 {/* Soil Moist */}
-                <section id="temperature" className="flex py-8">
+                <section id="temperature" className="flex py-8 justify-around">
                     <h2>Soil Moist</h2>
+                    <p className='mr-10'>100</p>
+                    <p>200</p>
                 </section>
 
                 {/* Raining Chance */}
-                <section id="temperature" className="flex py-8">
+                <section id="temperature" className="flex py-8 justify-around">
                     <h2>Raining chance</h2>
+                    <p className='mr-10'>100</p>
+                    <p>200</p>
                 </section>
 
                 {/* Need to water ? */}
-                <section id="temperature" className="flex py-8">
+                <section id="temperature" className="flex py-8 justify-around">
                     <h2>Need to water ?</h2>
+                    <p className='mr-10'>100</p>
+                    <p>200</p>
                 </section>
             </div>
         </div>
