cff051f5a0742ca3d7f45193edcefa466550fbb9
natchy <natchjirawat@gmail.com>
2023-05-18 12:42:24 +0700

    gradient color & change text when small screen

--- frontend/components/main.tsx
+++ frontend/components/main.tsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState, useRef, Fragment } from "react";
 import Measurement from "./measurement"
 import PopUp from "./tools/modal"
 import Sensor from "./sensor"
-import { FaTree, FaWater } from "react-icons/fa";
+import { FaQuestion, FaTree, FaWater } from "react-icons/fa";
 import Modal1 from "./tools/modal1";
 
 const Main = () => {
-    const [showModal1 , setShowModal1] = useState(false);
+    const [showModal1, setShowModal1] = useState(false);
     const [currentDateTime, setCurrentDateTime] = useState(new Date());
     const [updatedDateTime, setUpdatedDateTime] = useState(new Date());
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
 
     useEffect(() => {
         const timer = setInterval(() => {
@@ -29,9 +30,20 @@ const Main = () => {
         }
     }, []) // updated value every 30 minutes (will change later)
 
+    useEffect(() => {
+        const checkScreenSize = () => {
+            setIsSmallScreen(window.innerWidth < 1280);
+        };
+
+        checkScreenSize();
+        window.addEventListener('resize', checkScreenSize);
+
+        return () => window.removeEventListener('resize', checkScreenSize);
+    }, []);
+
     return (
-        <div className="font-poppins bg-green-100 m-0 p-0 h-screen overflow-hidden">
-            <div className="fixed bg-green-300 top-0 w-[100%] z-20">
+        <div className="font-poppins bg-green-100 m-0 p-0 w-full h-screen overflow-hidden">
+            <div className="fixed bg-gradient-to-l from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% top-0 w-[100%] z-20">
                 <div className="container mx-auto flex justify-between items-center  py-4">
                     <div className="flex gap-1 items-center text-2xl font-bold mr-10">
                         <span className="italic">Plants</span>
@@ -45,15 +57,19 @@ const Main = () => {
                     </div>
                     <Fragment>
                         <div>
-                        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5" 
-                        onClick={() => 
-                        setShowModal1(true)}>
-                        How we tell status?
-                        </button>
+                            {!isSmallScreen ?
+                                <button className={`text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium items-center rounded-lg px-5 py-2.5 text-center mr-5 hover:scale-110 ease-in duration-300`}
+                                    onClick={() => setShowModal1(true)}>
+                                    How we tell status?
+                                </button>
+                                :
+                                <button className="w-12 h-12 rounded-full bg-blue-700 hover:bg-blue-800 shadow-lg text-white flex items-center justify-center hover:scale-110 ease-in duration-300">
+                                    <FaQuestion />
+                                </button>
+                            }
                         </div>
-                        <Modal1 isVisible = {showModal1} onClose ={() =>
-                        setShowModal1(false) } />
-                    {/* <PopUp/> */}
+                        <Modal1 isVisible={showModal1} onClose={() => setShowModal1(false)} />
+                        {/* <PopUp/> */}
                     </Fragment>
                 </div>
             </div>
--- frontend/components/tools/modal1.tsx
+++ frontend/components/tools/modal1.tsx
@@ -1,24 +1,25 @@
 import React, { Fragment, useState } from 'react';
 import { FaQuestion } from 'react-icons/fa';
 
-const Modal1 = ({ isVisible,onClose}: any) => {
-    if ( !isVisible )return null;
+const Modal1 = ({ isVisible, onClose }: any) => {
+  if (!isVisible) return null;
 
   return (
     <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex
-    justify-center items-center'>
-        <div className='w-[600px] flex flex-col'>
-            <button className='text-white text-xl
+    justify-center items-center transition-opacity duration-500 ease-in-out opacity-100'>
+      <div className='w-[600px] flex flex-col'>
+        <button className='text-white text-xl
             place-self-end' onClick={() => onClose()}
-            >X</button>
-            <div className='bg-white p-2
+        >X</button>
+        <div className='bg-white p-2
             rounded'>
-                only god will know how we're going to finish this project
-            </div>
+          only god will know how we're going to finish this project
         </div>
+      </div>
     </div>
   );
 };
+
 
 export default Modal1;
 
