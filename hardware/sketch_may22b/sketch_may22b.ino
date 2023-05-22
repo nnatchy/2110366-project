209119cd56f425f7bb58c1b7d52842555662e00d
NUT <natthasith.wir@gmail.com>
2023-05-23 04:30:04 +0700

    Update sketch_may22b.ino

--- hardware/sketch_may22b/sketch_may22b.ino
+++ hardware/sketch_may22b/sketch_may22b.ino
@@ -1,41 +1,88 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseESP8266.h>
 
-#define FIREBASE_HOST "embed-toilet-default-rtdb.asia-southeast1.firebasedatabase.app" // Your Firebase Project URL goes here without "http:" and "/"
-#define FIREBASE_AUTH "LSSpk0ijKUICj9KLXTjtU6IA8tUFLHdzbwriclCu" // Your secret key
-#define WIFI_SSID "Nut (Iphone)" // your WiFi SSID
-#define WIFI_PASSWORD "Nut12345" // your WiFi Password
-
+// Initialize FirebaseESP8266 library
 FirebaseData firebaseData;
+
+// Replace with your Firebase project's host
+#define FIREBASE_HOST "your-firebase-host.firebaseio.com"
+
+// Replace with your Firebase project's secret
+#define FIREBASE_AUTH "your-firebase-auth"
+
+// Replace with your network credentials
+#define WIFI_SSID "your_SSID"
+#define WIFI_PASSWORD "your_PASSWORD"
 
 void setup() {
   Serial.begin(115200);
   
-  // connect to wifi.
+  // Connect to WiFi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting");
+  Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
-    delay(500);
+    delay(300);
   }
   Serial.println();
-  Serial.print("connected: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("Wi-Fi connected");
   
+  // Connect to Firebase
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-
-  // Set max temperature for "yourDocumentId" in Firebase
-  Firebase.setInt(firebaseData, "/temperature/yourDocumentId/max", 120);
+  Serial.println("Connected to Firebase");
 }
 
 void loop() {
-  // Here you'd implement code to retrieve data from the STM32 and update Firebase.
+  // Update data in the Firebase database
+  if (Firebase.setInt(firebaseData, "/sensorData/temperature", random(20, 30))) {
+    Serial.println("Successfully updated data");
+  } else {
+    Serial.println("Failed to update data");
+    Serial.println("Reason: " + firebaseData.errorReason());
+  }
   
-  // You might want to update the current temperature for example, make sure the path matches your Firebase structure
-  int temperature = Firebase.getInt(firebaseData, "/temperature/yourDocumentId/current");
-  temperature = temperature + 1;
-
-  Firebase.setInt(firebaseData, "/temperature/yourDocumentId", temperature);
-  
-  delay(1000); // Wait for a second
+  delay(5000);
 }
+
+
+// #include <ESP8266WiFi.h>
+// #include <FirebaseESP8266.h>
+
+// #define FIREBASE_HOST "embed-toilet-default-rtdb.asia-southeast1.firebasedatabase.app" // Your Firebase Project URL goes here without "http:" and "/"
+// #define FIREBASE_AUTH "LSSpk0ijKUICj9KLXTjtU6IA8tUFLHdzbwriclCu" // Your secret key
+// #define WIFI_SSID "Nut (Iphone)" // your WiFi SSID
+// #define WIFI_PASSWORD "Nut12345" // your WiFi Password
+
+// FirebaseData firebaseData;
+
+// void setup() {
+//   Serial.begin(115200);
+  
+//   // connect to wifi.
+//   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+//   Serial.print("connecting");
+//   while (WiFi.status() != WL_CONNECTED) {
+//     Serial.print(".");
+//     delay(500);
+//   }
+//   Serial.println();
+//   Serial.print("connected: ");
+//   Serial.println(WiFi.localIP());
+  
+//   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+//   // Set max temperature for "yourDocumentId" in Firebase
+//   Firebase.setInt(firebaseData, "/temperature/yourDocumentId/max", 120);
+// }
+
+// void loop() {
+//   // Here you'd implement code to retrieve data from the STM32 and update Firebase.
+  
+//   // You might want to update the current temperature for example, make sure the path matches your Firebase structure
+//   int temperature = Firebase.getInt(firebaseData, "/temperature/yourDocumentId/current");
+//   temperature = temperature + 1;
+
+//   Firebase.setInt(firebaseData, "/temperature/yourDocumentId", temperature);
+  
+//   delay(1000); // Wait for a second
+// }
